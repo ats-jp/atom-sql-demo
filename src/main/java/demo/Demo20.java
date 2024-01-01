32e36d7e0abe584fd5e4964ea69c0eac7840b77a
ats.tchiba <ats.t.chiba@gmail.com>
2024-01-02 06:55:53 +0900

    update

---
+++ src/main/java/demo/Demo20.java
@@ -0,0 +1,45 @@
+package demo;
+
+import java.util.List;
+
+import jp.ats.atomsql.Atom;
+import jp.ats.atomsql.HalfAtom;
+import jp.ats.atomsql.Sandbox;
+import jp.ats.atomsql.annotation.Sql;
+import jp.ats.atomsql.annotation.SqlInterpolation;
+import jp.ats.atomsql.annotation.SqlProxy;
+
+public class Demo20 {
+
+	public static void main(String[] args) throws Exception {
+		Sandbox.execute(atomSql -> {
+			var proxy = atomSql.of(Proxy.class);
+
+			Sandbox.resultSet(r -> {
+				r.addRow(91);
+				r.addRow(92);
+				r.addRow(93);
+			});
+
+			proxy.select1().forEach(System.out::println);
+
+			proxy.select2().list().forEach(System.out::println);
+
+			proxy.select3().put(i -> i.select = proxy.select2()).list().forEach(System.out::println);
+		});
+	}
+
+	@SqlProxy
+	public interface Proxy {
+
+		@Sql("SELECT 1 FROM customer")
+		List<Integer> select1();
+
+		@Sql("SELECT 1 FROM customer")
+		Atom<Integer> select2();
+
+		@Sql("SELECT 1 FROM customer /*${select}*/")
+		@SqlInterpolation
+		HalfAtom<Integer, I1> select3();
+	}
+}
