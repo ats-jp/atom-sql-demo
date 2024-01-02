7f3792cda992b00396ac929f82d9ed97a1364164
ats.tchiba <ats.t.chiba@gmail.com>
2024-01-03 02:52:21 +0900

    update

--- src/main/java/demo/Demo18.java
+++ src/main/java/demo/Demo18.java
@@ -2,10 +2,12 @@ package demo;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 import jp.ats.atomsql.Sandbox;
 import jp.ats.atomsql.annotation.DataObject;
 import jp.ats.atomsql.annotation.Sql;
+import jp.ats.atomsql.annotation.SqlParameters;
 import jp.ats.atomsql.annotation.SqlProxy;
 
 public class Demo18 {
@@ -19,6 +21,12 @@ public class Demo18 {
 			proxy.selectByLong(null);
 
 			proxy.insert(null, null, null);
+
+			proxy.insert(p -> {
+				p.id = null;
+				p.name = null;
+				p.created = null;
+			});
 		});
 	}
 
@@ -36,6 +44,10 @@ public class Demo18 {
 
 		@Sql("INSERT INTO customer (id, name, created) VALUES (:id, :name, :created)")
 		int insert(Long id, String name, LocalDateTime created);
+
+		@Sql("INSERT INTO customer (id, name, created) VALUES (:id, :name, :created)")
+		@SqlParameters
+		int insert(Consumer<Demo18_P1> c);
 
 		@DataObject
 		public static class DataObjectImpl {
