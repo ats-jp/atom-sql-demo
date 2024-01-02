38cb647bc5de0a5b49449d173f149ce934fd9c44
ats.tchiba <ats.t.chiba@gmail.com>
2024-01-03 01:05:03 +0900

    update

--- src/main/java/demo/Demo19.java
+++ src/main/java/demo/Demo19.java
@@ -2,13 +2,16 @@ package demo;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 import jp.ats.atomsql.Csv;
 import jp.ats.atomsql.Sandbox;
 import jp.ats.atomsql.annotation.DataObject;
 import jp.ats.atomsql.annotation.EnumValue;
 import jp.ats.atomsql.annotation.Sql;
+import jp.ats.atomsql.annotation.SqlParameters;
 import jp.ats.atomsql.annotation.SqlProxy;
+import jp.ats.atomsql.annotation.TypeHint;
 
 public class Demo19 {
 
@@ -21,6 +24,17 @@ public class Demo19 {
 			proxy.selectByEnum2(Enum2.CONST3);
 
 			proxy.selectByEnum1(null);
+
+			proxy.selectByParameters(p -> p.type = Enum1.CONST1);
+
+			proxy.selectByParameters(p -> p.type = null);
+
+			proxy.selectByCsv(p -> p.types = Csv.of(Enum1.CONST1));
+
+			proxy.selectWithHint(p -> {
+				p.type = Enum1.CONST1;
+				p.csv = Csv.of(Enum1.CONST1, Enum1.CONST3);
+			});
 
 			Sandbox.resultSet(r -> {
 				r.setColumnNames("e1", "e2");
@@ -68,6 +82,21 @@ public class Demo19 {
 
 		@Sql("SELECT * FROM customer WHERE type IN (:types)")
 		List<DataObjectImpl> selectByCsv(Csv<Enum1> types);
+
+		@Sql("SELECT * FROM customer WHERE type = :type/*Demo19.Enum1*/")
+		@SqlParameters
+		List<DataObjectImpl> selectByParameters(Consumer<Demo19_P1> c);
+
+		@Sql("SELECT * FROM customer WHERE type IN (:types/*CSV<Demo19.Enum1>*/)")
+		@SqlParameters
+		List<DataObjectImpl> selectByCsv(Consumer<Demo19_P2> c);
+
+		@Sql("SELECT * FROM customer WHERE type = :type AND csv IN :csv")
+		@SqlParameters(typeHints = {
+			@TypeHint(name = "type", type = "Demo19.Enum1"),
+			@TypeHint(name = "csv", type = "CSV", typeArgument = "Demo19.Enum1"),
+		})
+		List<DataObjectImpl> selectWithHint(Consumer<Demo19_P3> c);
 
 		@DataObject
 		public static class DataObjectImpl {
